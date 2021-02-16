e72ef5b569ba328439d1d8ff0b47509aae4efbe4
sidd sirius <sidd.benoit@gmail.com>
2021-02-16 08:28:38 -0500

    L3

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'A warm pair of fuzzy socks.',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://v3.vuejs.org/'
         }
     }
 })
