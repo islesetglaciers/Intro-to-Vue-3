3399896cb64f8c2fddd28eedf9ae69112ad5589d
sidd sirius <sidd.benoit@gmail.com>
2021-02-16 10:03:06 -0500

    L10

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -36,8 +36,15 @@ app.component('product-display', {
           class="button" 
           :class="{ disabledButton: !inStock }" 
           :disabled="!inStock" 
-          v-on:click="addToCart">
+          @click="addToCart">
           Add to Cart
+        </button>
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          @click="removeFromCart">
+          Remove Item
         </button>
       </div>
     </div>
@@ -56,10 +63,13 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id);
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      removeFromCart() {
+          this.$emit('remove-from-cart');
       }
   },
   computed: {
