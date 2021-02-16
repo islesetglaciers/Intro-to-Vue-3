a52887a50f2ce3061b6dd658d5bcef39f333dfe1
sidd sirius <sidd.benoit@gmail.com>
2021-02-16 09:49:11 -0500

    L9 challenge

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -1,5 +1,13 @@
 app.component('product-details', {
     props: {
-        details
-    }
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+    <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>
 })
